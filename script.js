25d536af786eb7e09367d9affde0b92987dc05ec
Jason <jsonballadares@gmail.com>
2026-04-10 04:31:34 +0000

    landing page resume for now

--- script.js
+++
@@ -1,8 +0,0 @@
-let counter = 0;
-const counterElement = document.getElementById('counter');
-const button = document.getElementById('increment');
-
-button.addEventListener('click', () => {
-    counter++;
-    counterElement.textContent = counter;
-});
